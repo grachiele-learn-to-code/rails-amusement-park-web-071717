66a61f41ee802a19124cd0e61d9144d6067ef9a0
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-08-17 16:15:49 -0400

    garbage

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,11 +4,11 @@ class RidesController < ApplicationController
   end
 
   def create
-    # @attraction = Attraction.find_by(id: params[:id])
-		# @user = User.find_by(id: session[:user_id])
+    @attraction = Attraction.find_by(id: params[:id])
+		@user = User.find_by(id: session[:user_id])
 		@ride = Ride.create(user: @user, attraction: @attraction)
     # @ride.take_ride
-    redirect_to user_path
+    redirect_to user_path(@user)
   end
 
 end
