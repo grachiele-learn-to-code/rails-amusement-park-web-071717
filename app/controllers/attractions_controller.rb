943385a8412a46bb51bd6efee17c8d3c457b2128
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-08-17 15:39:17 -0400

    added attraction controller and links to rides.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,25 +2,28 @@ class AttractionsController < ApplicationController
 	before_action :admin?, except: [:index, :show]
 
 
-	def index	
+	def index
+		@attractions = Attraction.all
 	end
 
-	def show	
+	def show
+		@attraction = Attraction.find_by(id: params[:id])
+		# @user = User.find_by(id: session[:user_id])
 	end
 
-	def new	
+	def new
 	end
 
-	def create	
+	def create
 	end
 
-	def edit	
+	def edit
 	end
 
-	def update	
+	def update
 	end
 
-	def destroy	
+	def destroy
 	end
 
 
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,2 +1,13 @@
 class RidesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @attraction = Attraction.find_by(id: params[:id])
+		@user = User.find_by(id: session[:user_id])
+		@ride = Ride.new(user: @user, attraction: @attraction)
+    @ride.take_ride
+  end
+
 end
