980de8b33ac5c1d2fdca6aad592fdee56f0a9f4d
Neil Steiner <npsteiner@gmail.com>
2017-08-17 15:05:16 -0400

    began working on attractions controller and views, as well as linking to them from users show page

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,2 +1,35 @@
 class AttractionsController < ApplicationController
+	before_action :admin?, except: [:index, :show]
+
+
+	def index	
+	end
+
+	def show	
+	end
+
+	def new	
+	end
+
+	def create	
+	end
+
+	def edit	
+	end
+
+	def update	
+	end
+
+	def destroy	
+	end
+
+
+
+	private
+
+	def admin?
+		user = User.find_by(id: session[:user_id])
+		user.admin
+	end
+
 end
